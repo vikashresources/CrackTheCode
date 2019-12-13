e9886c986437b0140991b82d1c7e95d52b6a5f97
vikash choudhari <vikash.choudhari07@gmail.com>
2019-12-13 09:06:53 +0530

    Space and time complexity

--- BigO.py
+++ BigO.py
@@ -1,3 +1,10 @@
+'''
+Complexity - O(n) time and O(n) space
+For stack, it would require O(n) space and as if we need to create a array of size n.
+Each of these calls is added to the call stack and takes up actual memory
+'''
+
+
 def sum(n):
     if n <= 0:
         return 0
@@ -6,5 +13,21 @@ def sum(n):
 
 print(sum(5))
 
+'''In below function, there will be O(n) calls to pair_sum, however those calls do not exit simultaneously on stack & 
+thus space complexity of O(1) '''
 
 
+def pair_sum(a, b):
+    return a + b
+
+
+def pair_sum_sequence(n):
+    total = 0
+    if n <= 0:
+        return 0
+    for i in range(n):
+        total += pair_sum(i, i + 1)
+    return total
+
+
+print(pair_sum_sequence(5))
