88e3c84e1d9eca548cff24aa2373101c29364b8e
vikash choudhari <vikash.choudhari07@gmail.com>
2020-01-03 10:45:32 +0530

    python Towers Of Hanoi

--- TowersOfHanoi.py
+++ TowersOfHanoi.py
@@ -3,10 +3,8 @@ def TowersOfHanoi(no_of_disc, from_peg, to_peg, aux_peg):
         print("Move disk 1 from peg", from_peg, "to peg", to_peg)
         return
     else:
-        '''move n-1 discs from A To B Using C'''
         TowersOfHanoi(no_of_disc - 1, from_peg, aux_peg, to_peg)
         print("Move disk", no_of_disc, "from peg", from_peg, "to peg", to_peg)
-        '''move n-1 discs from B to C using A'''
         TowersOfHanoi(no_of_disc - 1, aux_peg, to_peg, from_peg)
 
 
